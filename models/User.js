34baef1456073dd7b35e320cd671e9861dece080
Yousif-Mohammedx <126144587+Yousif-Mohammedx@users.noreply.github.com>
2024-03-10 08:59:09 +0000

    video 14 done

---
+++ models/User.js
@@ -0,0 +1,28 @@
+import { Schema, model } from "mongoose";
+import { hash } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+const UserSchema = new Schema({
+    avatar: { type: String, default: "" },
+    name: { type: String, requierd: true },
+    email: { type: String, requierd: true },
+    password: { type: String, requierd: true },
+    verified: { type: Boolean, default: false },
+    verificationCode: { type: String, requierd: false },
+    admin: { type: Boolean, default: false },
+}, { timestamps: true }
+);
+
+UserSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        this.password = await hash(this.password, 10);
+        return next()
+    }
+    return next()
+})
+
+UserSchema.methods.generateJWT = async function () {
+    return await sign({ id: this._id }, process.env.JWT_SECRET, { expiresIn: '30d' })
+}
+const User = model("User", UserSchema);
+export default User;
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
 
+// Routes
+import userRoutes from './routes/userRoutes';
+
 dotenv.config();
 
 const app = express();
@@ -13,6 +16,7 @@ app.get("/", (req, res) => {
     res.send("Server is running...");
 });
 
+app.use('/api/users', userRoutes)
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
