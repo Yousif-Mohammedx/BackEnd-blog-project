2d77c6c46e810418ebcc482ebc8df249115ae70b
Yousif-Mohammedx <126144587+Yousif-Mohammedx@users.noreply.github.com>
2024-03-11 07:26:28 +0000

    video 15 (done)

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,12 +1,13 @@
 import User from '../models/User';
 
-export const registerUser = async (req, res) => {
+export const registerUser = async (req, res, next) => {
     try {
         const { name, email, password } = req.body;
         // check whether the user exists or not
         let user = await User.findOneAndUpdate({ email });
         if (user) {
-            return res.status(400).json({ message: "User have already registerd" })
+            // return res.status(400).json({ message: "User have already registerd" })
+            throw new Error("User have already registered")
         }
         user = await User.create({
             name, email, password,
@@ -21,8 +22,8 @@ export const registerUser = async (req, res) => {
             token: await user.generateJWT(),
         });
     } catch (error) {
-        return res.status(500).json({message: "Something"});
+        next(error);
     }
 };
 
-export{registerUser};
+export { registerUser };
